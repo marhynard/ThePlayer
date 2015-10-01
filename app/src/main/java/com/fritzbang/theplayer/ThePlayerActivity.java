aa7aa667574825cd17b52dd333392a4b0799b146
marhynard <fritz.gone@gmail.com>
2015-09-30 22:56:59 -0400

    added files to include the downlow

--- app/src/main/java/com/fritzbang/theplayer/ThePlayerActivity.java
+++ app/src/main/java/com/fritzbang/theplayer/ThePlayerActivity.java
@@ -6,9 +6,12 @@ import java.util.ArrayList;
 import java.util.Locale;
 
 import android.annotation.SuppressLint;
+import android.app.ActionBar;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.AlertDialog;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
@@ -83,6 +86,8 @@ public class ThePlayerActivity extends Activity {
     final CharSequence[] sortType_radio = {"Title", "Album", "Artist", "Track"};
     NoisyAudioStreamReceiver myNoisyAudioStreamReceiver = new NoisyAudioStreamReceiver();
 
+    // TODO add a navigation drawer to handle the player functions (so it persists on each screen)
+    // TODO implement actionbar to include the tabs
     // TODO update the files system within the app to have a podcast location and music location and book location
     // TODO standardize the lists between the Podcasts and Music tracks
     // TODO when loading the playlist load the correct files either podcasts,music, or both
@@ -1079,5 +1084,49 @@ public class ThePlayerActivity extends Activity {
         }
         return directorySize;
     }
-
+//    public static class TabListener<T extends Fragment> implements ActionBar.TabListener {
+//        private Fragment mFragment;
+//        private final Activity mActivity;
+//        private final String mTag;
+//        private final Class<T> mClass;
+//
+//        /** Constructor used each time a new tab is created.
+//         * @param activity  The host Activity, used to instantiate the fragment
+//         * @param tag  The identifier tag for the fragment
+//         * @param clz  The fragment's Class, used to instantiate the fragment
+//         */
+//        public TabListener(Activity activity, String tag, Class<T> clz) {
+//            mActivity = activity;
+//            mTag = tag;
+//            mClass = clz;
+//        }
+//
+//    /* The following are each of the ActionBar.TabListener callbacks */
+//        @Override
+//        public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+//            // Check if the fragment is already initialized
+//            if (mFragment == null) {
+//                // If not, instantiate and add it to the activity
+//                mFragment = Fragment.instantiate(mActivity, mClass.getName());
+//                ft.add(android.R.id.content, mFragment, mTag);
+//            } else {
+//                // If it exists, simply attach it in order to show it
+//                ft.attach(mFragment);
+//            }
+//        }
+//        @Override
+//        public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+//            if (mFragment != null) {
+//                // Detach the fragment, because another one is being attached
+//                ft.detach(mFragment);
+//            }
+//        }
+//        @Override
+//        public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+//            // User selected the already selected tab. Usually do nothing.
+//        }
+//
+//
+//
+//    }
 }
